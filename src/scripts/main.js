cb5d52b78d63d0b47383506b0c989fc2223dac68
Dmitrii Damyan <dmitriignc@gmail.com>
2024-12-17 15:08:26 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,12 @@
 'use strict';
 
-const input = document.querySelectorAll('input, type');
+const input = document.querySelectorAll('input');
 const label = [];
 
 console.log(input[0].id);
 for (let i = 0; i < input.length; i++) {
   label.push(document.createElement('label'));
-  label[i].before(label[i]);
+  input[i].before(label[i]);
   input[i].className = 'field-label';
   label[i].htmlFor = input[i].id
   label[i].textContent = input[i].name.slice(0,1).toUpperCase() + input[i].name.slice(1);
